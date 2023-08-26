474c196305994b3bb880c2041d96a35506d67834
zloblin <goblin86@mail.ru>
2023-08-26 15:25:14 +0300

    Update PetShelterApplicationTests.java
    Added logger for PetShelterApplicationTests.java

--- src/test/java/com/example/petshelter/PetShelterApplicationTests.java
+++ src/test/java/com/example/petshelter/PetShelterApplicationTests.java
@@ -2,9 +2,12 @@ package com.example.petshelter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootTest
-class PetShelterApplicationTests {
+@SpringBootApplication
+public class PetShelterApplication {
+	private final Logger logger = LoggerFactory.getLogger(PetShelterApplication.class);
 
 	@Test
 	void contextLoads() {
