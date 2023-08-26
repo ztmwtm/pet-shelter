7ec23a57fa37d3971d57839ccfc9390807ae9218
zloblin <goblin86@mail.ru>
2023-08-26 14:01:50 +0300

    Update PetShelterApplication.java
    Added logger to PetShelterApplication.java

--- src/main/java/com/example/petshelter/PetShelterApplication.java
+++ src/main/java/com/example/petshelter/PetShelterApplication.java
@@ -2,9 +2,12 @@ package com.example.petshelter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class PetShelterApplication {
+	private final Logger logger = LoggerFactory.getLogger(PetShelterApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetShelterApplication.class, args);
