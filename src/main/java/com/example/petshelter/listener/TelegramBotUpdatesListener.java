a0653884177cb7f096922d9336609467f4cb0e81
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-08-29 13:43:08 +0700

    Add TG bot configuration settings

---
+++ src/main/java/com/example/petshelter/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,31 @@
+package com.example.petshelter.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            System.out.println(update);
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
